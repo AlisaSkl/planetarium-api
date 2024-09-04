2bca9b2e2ddcafe97285ff0d18573648739e6811
Alisa <alisasklrv@gmail.com>
2024-09-04 19:31:13 +0300

    create ShowTheme serializer, view and url

---
+++ planetarium/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetaryDome,
+    ShowSession,
+    Reservation,
+    Ticket
+)
+
+
+class ShowThemeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ShowTheme
+        fields = ("id", "name")
+
+
+class PlanetaryDomeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PlanetaryDome
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+class AstronomyShowListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "title", "description")
+
+
+
+
+
+
--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import ShowTheme
+from .serializers import ShowThemeSerializer
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
