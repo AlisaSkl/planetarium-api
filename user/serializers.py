da4ffc71434a97bb9818dce03ff651e908fbb8f5
Alisa Skliarova <alisasklrv@gmail.com>
2025-04-05 17:55:00 +0200

    create user view

--- user/serializers.py
+++ user/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
-class UserSerializers(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "username", "email", "password", "is_staff")
